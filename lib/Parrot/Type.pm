dfdbac6e1911e5f4759a45192c27afa21709c423
denisboyun <denisboyun@gmail.com>
2013-07-07 11:38:14 +0300

    commencement of work on different build and test systems

--- lib/Parrot/Type.pm
+++ lib/Parrot/Type.pm
@@ -3,17 +3,26 @@ package Parrot::Type;
 use v5.14;
 use Moose::Util::TypeConstraints;
 
+use constant {
+    winxed            => 'Winxed',
+    nqp               => 'NQP (Not Quite Perl 6)',
+    pir               => 'PIR (Parrot Intermediate Representation)',
+    perl5             => 'Perl 5',
+    rosella_winxed    => 'Rosella (Winxed)',
+    rosella_nqp       => 'Rosella (NQP)'
+};
+
 enum 'Build_Systems', [
-                       'Winxed',
-                       'NQP (Not Quite Perl 6)',
-                       'PIR (Parrot Intermediate Representation)',
-                       'Perl 5'
+                       winxed,
+                       nqp,
+                       pir,
+                       perl5
                       ];
 
 enum 'Test_Systems', [
-                       'Rosella (Winxed)',
-                       'Rosella (NQP)',
-                       'Perl 5'
+                       rosella_winxed,
+                       rosella_nqp,
+                       perl5
                      ];
 
 no Moose::Util::TypeConstraints;
