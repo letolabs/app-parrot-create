8bddaf8ac1a7fef7d167b803f43ff5f3e58251b2
denisboyun <denisboyun@gmail.com>
2013-08-27 19:57:06 +0300

    Update documentation

--- lib/Parrot/Library.pm
+++ lib/Parrot/Library.pm
@@ -17,3 +17,61 @@ method init($name, $build_system, $test_system) {
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+    Parrot::Library - Moose class for work with parrot library.
+
+=head1 SYNOPSIS
+
+    #simple using
+    my $lib = Parrot::Library->new();
+    $lib->init($name, $builder, $harness);
+    $lib->template($hll_template);
+    
+    #and return archive
+    $archive_path = $lib->generate();
+
+=head1 METHODS
+
+=head3 init
+
+=head4 Description:
+    init all information about Parrot Library;
+=cut
+
+=head4 Args:
+    $name,
+    $build_system,
+    $test_system;
+=cut
+
+=head4 Retuns:
+    1;
+=cut
+
+=head1 DESCRIPTION
+    
+    Class is using for work with Parrot Library objects. It's implementing a Parrot::Base role and expanding its api.
+
+=head1 BUGS
+
+    If you find bugs please report us.
+    https://github.com/letolabs/app-parrot-create 
+
+=head1 AUTHORS
+
+    Jonathan Leto duke@leto.net
+    Denis Boyun denisboyun@gmail.com
+
+=head1 COPYRIGHT AND LICENSE
+
+    Copyright (C) 2013, Parrot Foundation.
+    
+    This program is free software, you can redistribute it and/or modify it under the terms of the Artistic License version 2.0
+
+=cut
